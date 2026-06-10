df1da4cd2eb1e0204c3a804c563a7a0ed3e087fe
aaron-jencks <aaronjencks@aol.com>
2026-06-10 10:43:28 -0400

    fixed wrapper definitions

--- automation/bearssl-0.6/ccopy/v3/uut/wrapper.c
+++ automation/bearssl-0.6/ccopy/v3/uut/wrapper.c
@@ -4,12 +4,13 @@
 #include "inner.h"
 
 uint64_t timed_call_uut(
+    uint32_t key,
     trial_context_t ctx
 ) {
     size_t i;
     uint64_t start, end, duration;
     start = monotonic_ns();
-    br_ccopy(*ctx.key, ctx.dest, ctx.data, *ctx.data_len);
+    br_ccopy(key, ctx.dest, ctx.data, *ctx.data_len);
     end = monotonic_ns();
     return end - start;
 }
