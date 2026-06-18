fdcce3663f3808afecdbd28f677fb02bbca09c58
aaron-jencks <aaronjencks@aol.com>
2026-06-18 17:19:03 -0400

    removed old code

--- automation/simulation/api/common.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-import pandas as pd
-
-
-class DeploymentController(ABC):
-    @abstractmethod
-    def deploy_test_case(self, code: str, **kwargs) -> pd.DataFrame:
-        pass
