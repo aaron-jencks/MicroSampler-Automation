2ede0c7d1129867546176a82f2d80375b2d6b61c
aaron-jencks <aaronjencks@aol.com>
2026-06-24 17:27:55 -0400

    fixed no apps or keys

--- automation/tests/test_microsampler_core_deployment.py
+++ automation/tests/test_microsampler_core_deployment.py
@@ -9,7 +9,11 @@ class MicroSamplerCoreDeploymentTestCase(unittest.TestCase):
     def test_deploy_attack_fixture(self):
         config = parse_configs([])
         sm = MicroSamplerCoreDeploymentMachine.from_config_file(config.microsampler.core_deployment_qsm, ctx=config)
-        self.assertIsNone(sm.loop_w_config(MicroSamplerRunConfiguration()))
+        self.assertIsNone(sm.loop_w_config(MicroSamplerRunConfiguration(
+            suite="bearssl_synthetic",
+            apps=["v1"],
+            keys=["0xaa"]
+        )))
 
 
 if __name__ == '__main__':
