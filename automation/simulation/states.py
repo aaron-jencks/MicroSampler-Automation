4071becba87f17f3e4e4da815faef89145ae9af8
aaron-jencks <aaronjencks@aol.com>
2026-06-30 12:13:55 -0400

    added log outputs to subprocess calls when possible

--- automation/simulation/states.py
+++ automation/simulation/states.py
@@ -1,5 +1,4 @@
 from abc import ABC
-from dataclasses import dataclass
 from enum import StrEnum
 import logging
 import os
@@ -25,13 +24,21 @@ class DeploymentState(State, ABC):
         ctx.queue.append(state.value)
 
 
+SP_FILE = Optional[Union[IO[Any], int, Path]]
+
+
 class SubprocessDeploymentState(DeploymentState, ABC):
     def check_subprocess_output(
             self, ctx: StateContext, sp_output: sp.CompletedProcess,
-            err: Type[SubprocessError], next_state: StrEnum
+            err: Type[SubprocessError], next_state: StrEnum,
+            stdout: Optional[Path] = None, stderr: Optional[Path] = None
     ):
         if sp_output.returncode != 0:
-            e = err(sp_output.returncode, sp_output.stdout, sp_output.stderr)
+            e = err(
+                sp_output.returncode,
+                stdout if stdout else sp_output.stdout,
+                stderr if stderr else sp_output.stderr
+            )
             logger.warning(f"subprocess failed, program output:\n\n{e.format_error()}")
             ctx.stop(e)
         else:
@@ -40,9 +47,9 @@ class SubprocessDeploymentState(DeploymentState, ABC):
     def run_subprocess(
             self,
             args: Union[Iterable[str], str],
-            stdin: Optional[Union[IO[Any], int]] = None,
-            stdout: Optional[Union[IO[Any], int]] = None,
-            stderr: Optional[Union[IO[Any], int]] = None,
+            stdin: SP_FILE = None,
+            stdout: SP_FILE = None,
+            stderr: SP_FILE = None,
             shell: bool = False,
             env_overrides: Optional[Dict[str, str]] = None,
             inherit_env: bool = True,
@@ -52,20 +59,39 @@ class SubprocessDeploymentState(DeploymentState, ABC):
         env = os.environ.copy() if inherit_env else {}
         if env_overrides:
             env.update(env_overrides)
-        return sp.run(
-            args,
-            stdin=stdin, stdout=stdout, stderr=stderr,
-            shell=shell, check=check_output,
-            env=env, cwd=cwd
-        )
+        csi, cso, cse = False, False, False
+        if isinstance(stdin, Path):
+            stdin = open(stdin, 'r')
+            csi = True
+        if isinstance(stdout, Path):
+            stdout = open(stdout, 'w+')
+            cso = True
+        if isinstance(stderr, Path):
+            stderr = open(stderr, 'w+')
+            cse = True
+        try:
+            sp_out = sp.run(
+                args,
+                stdin=stdin, stdout=stdout, stderr=stderr,
+                shell=shell, check=check_output,
+                env=env, cwd=cwd
+            )
+        finally:
+            if csi:
+                stdin.close()
+            if cso:
+                stdout.close()
+            if cse:
+                stderr.close()
+        return sp_out
 
     def run_checked_subprocess(
             self, ctx: StateContext,
             err: Type[SubprocessError], next_state: StrEnum,
             args: Union[Iterable[str], str],
-            stdin: Optional[Union[IO[Any], int]] = None,
-            stdout: Optional[Union[IO[Any], int]] = None,
-            stderr: Optional[Union[IO[Any], int]] = None,
+            stdin: SP_FILE = None,
+            stdout: SP_FILE = None,
+            stderr: SP_FILE = None,
             shell: bool = False,
             env_overrides: Optional[Dict[str, str]] = None,
             inherit_env: bool = True,
@@ -74,4 +100,7 @@ class SubprocessDeploymentState(DeploymentState, ABC):
         run_out = self.run_subprocess(
             args, stdin, stdout, stderr, shell, env_overrides, inherit_env, False, cwd
         )
-        self.check_subprocess_output(ctx, run_out, err, next_state)
+        self.check_subprocess_output(
+            ctx, run_out, err, next_state,
+            stdout=stdout if isinstance(stdout, Path) else None, stderr=stderr if isinstance(stderr, Path) else None
+        )
