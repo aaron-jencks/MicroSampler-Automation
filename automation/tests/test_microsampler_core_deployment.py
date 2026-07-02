7c638a0ca3739d8946e8dd2e4b26887a605ebde8
aaron-jencks <aaronjencks@aol.com>
2026-07-02 11:24:10 -0400

    fixed gcc version test

--- automation/tests/test_microsampler_core_deployment.py
+++ automation/tests/test_microsampler_core_deployment.py
@@ -34,8 +34,10 @@ class MicroSamplerCoreDeploymentTestCase(unittest.TestCase):
         logger.debug("testing cross-compiler version")
         version_output = sp.run(["riscv64-unknown-elf-gcc", "--version"], capture_output=True, text=True)
         self.assertEqual(version_output.returncode, 0, "riscv64-unknown-elf-gcc did not print version successfully")
-        version_text = version_output.stderr.splitlines(keepends=False)[-1]
-        self.assertEqual(version_text, "gcc version 15.2.0 (g5115c7e44)", "gcc version expected to be 15.2.0 (g5115c7e44), otherwise expected pc addresses won't match")
+        version_text = version_output.stderr.splitlines(keepends=False)
+        self.assertGreater(len(version_text), 0)
+        version_text = version_text[0]
+        self.assertEqual(version_text, "riscv64-unknown-elf-gcc (g5115c7e44) 15.2.0", "gcc version expected to be 15.2.0 (g5115c7e44), otherwise expected pc addresses won't match")
         logger.debug("testing cross-compiler features")
         with tempfile.TemporaryDirectory() as d:
             src = Path(d) / "test.c"
