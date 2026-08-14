ea15fa74751725e77cc36224dcc3519ce53ae92b
aaron-jencks <aaronjencks@aol.com>
2026-08-14 14:16:30 -0400

    removed bulk comments

--- automation/simulation/microsampler/core/states.py
+++ automation/simulation/microsampler/core/states.py
@@ -185,77 +185,6 @@ class MicroSamplerSimulationState(MicroSamplerCoreStepState):
 class MicroSamplerParseState(MicroSamplerCoreStepState):
     def execute(self, ctx: MicroSamplerLoopContext):
         logger.debug("starting microsampler parse")
-        # key = ctx.context.run_config.keys[ctx.context.current_key_index]
-        #
-        # args = [key]
-        #
-        # suite = ctx.context.run_config.suite
-        # app = ctx.context.run_config.apps[ctx.context.current_app_index]
-        #
-        # pc_config = ctx.context.pc_addresses
-        # args.extend([
-        #     f"0x{pc_config.roi_start:010x}",
-        #     f"0x{pc_config.roi_end:010x}",
-        #     f"0x{pc_config.calling_address:010x}",
-        #     f"0x{pc_config.start_address:010x}",
-        #     f"0x{pc_config.return_address:010x}"
-        # ])
-        #
-        # # if suite == "microbench":
-        # #     if app == "ct_ccopy":
-        # #         args.extend([
-        # #             "0x008000010e", "0x0080000124", "0x0080000196", "0x0080000130", "0x008000019a"
-        # #         ])
-        # #     else:
-        # #         ctx.stop(ValueError(f"app {app} not supported"))
-        # #         return
-        # # elif suite == "bearssl_synthetic":
-        # #     if app == "v1":
-        # #         args.extend([
-        # #             "0x0000010128", "0x000001012c", "0x00000106d2", "0x000001022c", "0x00000106d6"
-        # #         ])
-        # #     elif app == "v1_warmup":
-        # #         args.extend([
-        # #             "0x000001014a", "0x000001014e", "0x00000106f4", "0x000001024e", "0x00000106f8"
-        # #         ])
-        # #     elif app == "v1_fence":
-        # #         args.extend([
-        # #             "0x0000010128", "0x000001012c", "0x00000107a4", "0x000001022c", "0x00000107a8"
-        # #         ])
-        # #     elif app == "v2":
-        # #         args.extend([
-        # #             "0x000001012c", "0x0000010130", "0x0000010882", "0x000001021a", "0x0000010886"
-        # #         ])
-        # #     elif app == "v2_warmup":
-        # #         args.extend([
-        # #             "0x0000010152", "0x0000010156", "0x00000108a8", "0x0000010240", "0x00000108ac"
-        # #         ])
-        # #     elif app == "v2_fence":
-        # #         args.extend([
-        # #             "0x000001012c", "0x0000010130", "0x000001095c", "0x000001021a", "0x0000010960"
-        # #         ])
-        # #     elif app == "v3":
-        # #         args.extend([
-        # #             "0x0000010128", "0x000001012c", "0x000001052e", "0x000001023c", "0x0000010532"
-        # #         ])
-        # #     elif app == "v3_warmup":
-        # #         args.extend([
-        # #             "0x000001014a", "0x000001014e", "0x0000010550", "0x000001025e", "0x0000010554"
-        # #         ])
-        # #     elif app == "v3_fence":
-        # #         args.extend([
-        # #             "0x0000010128", "0x000001012c", "0x0000010600", "0x000001023c", "0x0000010604"
-        # #         ])
-        # #     else:
-        # #         ctx.stop(ValueError(f"app {app} not supported"))
-        # #         return
-        # # else:
-        # #     ctx.stop(ValueError(f"suite {suite} not supported"))
-        # #     return
-        #
-        # args.extend([
-        #     suite, app, str(ctx.context.run_config.iterations), ctx.context.run_config.design,
-        # ])
 
         self.run_microsampler_checked_subprocess(
             ctx, MicroSamplerParsingError, MicroSamplerCoreDeploymentState.STATS,
@@ -268,15 +197,5 @@ class MicroSamplerStatsState(MicroSamplerCoreStepState):
         logger.debug("starting microsampler stats execution")
         self.run_microsampler_checked_subprocess(
             ctx, MicroSamplerStatsError, MicroSamplerCoreDeploymentState.LOOP_CHECK,
-            # [
-            #     ctx.context.run_config.keys[ctx.context.current_key_index],
-            #     ctx.context.run_config.suite,
-            #     ctx.context.run_config.apps[ctx.context.current_app_index],
-            #     str(ctx.context.run_config.phi),
-            #     str(ctx.context.run_config.alpha),
-            #     str(ctx.context.run_config.window),
-            #     str(ctx.context.run_config.iterations),
-            #     ctx.context.run_config.design,
-            # ],
             "launch_stats.log", do_stats
         )
