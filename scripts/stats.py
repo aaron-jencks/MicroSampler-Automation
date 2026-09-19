0806034f80a0f20baf01eb152f1be67a4b279fdd
Josh Bodner <jbodner09@outlook.com>
2026-09-19 12:08:20 -0400

    print 2

--- scripts/stats.py
+++ scripts/stats.py
@@ -117,7 +117,8 @@ for x in range(len(loops)):
     fetch = loops[x][0].fetch
     retire = loops[x][-1].retire
     print(f">>>loop {x}: fetch: {fetch}, retire: {retire}")
-    print(state for state in states)
+    for state in range(len(states)):
+        print(state)
     loopsUArch.append([state for state in states if state.cycle_begin >= fetch and state.cycle_begin <= retire])
 
 rcParams['font.family'] = 'sans-serif'
