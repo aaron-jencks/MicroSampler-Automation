9b033ab604bf8743e462e85ed171e06797aca044
Josh Bodner <jbodner09@outlook.com>
2026-09-19 17:29:09 -0400

    print 16

--- scripts/stats.py
+++ scripts/stats.py
@@ -114,7 +114,8 @@ diff = {}
 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 print(f"Total loop length: {len(loops)}")
 for x in range(len(loops)):
-    print(loops[x])
+    for y in range(len(loops[x]))
+        print(loops[x][y])
 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
     
     
