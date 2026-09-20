92a335430530c1adf666a60af72ba4604d6efcac
Josh Bodner <jbodner09@outlook.com>
2026-09-19 21:53:04 -0400

    print 26

--- scripts/stats.py
+++ scripts/stats.py
@@ -39,7 +39,12 @@ def uarch_diff(component, theta_lst, axs, _phi, _alpha):
             else:
                 obsrv[dclass].append(theta_lst[dclass][component][sidx][1])
                 stats[j].append((dclass, theta_lst[dclass][component][sidx][1]))
-              
+              stateString = f"{theta_lst[dclass][component][sidx][1]:.2f}\t{1 if theta_lst[dclass][component][sidx][1] >= _phi else 0}" #(
+                  #f"{dclass}\t{theta_lst[dclass][component][sidx][0].cycle_begin}\t"
+                  
+                  #f"\t{1 if theta_lst[dclass][component][sidx][1] <= _alpha else 0}"
+              #)
+              print(stateString)
 
     obsrv_candidates = {k: [] for k in theta_lst.keys()}
     for dclass in theta_lst:
