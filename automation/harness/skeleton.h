64afbaf934fa9bf07dfe050e75a3a36afeca3fdc
aaron-jencks <aaronjencks@aol.com>
2026-09-19 21:49:23 -0400

    fixed stale documentation

--- automation/harness/skeleton.h
+++ automation/harness/skeleton.h
@@ -16,17 +16,16 @@ global_setup(&global_context);
 
 for(size_t i = 0; i < iterations; i++) {
     iteration_keys[i] = (uint32_t)GENERATE_RANDOM_KEY;
-    bench_context_t run_context = create_context(&global_context, iteration_keys[i], i);
+    bench_context_t run_context = create_context(&global_context, i);
     trial_setup(&run_context);
     for(size_t ki = 0; ki < 32; ki++) {
         uint32_t bit = (uint32_t)((iteration_keys[i] >> ki) & 0x1);
         trial_context_t trial_context = create_default_trial_context(
-            bit,
             BUFFER_SIZE
         );
         trial_inner_setup(&run_context, &trial_context);
         helper_start(&run_context);
-        iteration_durations[i*32+ki] = timed_call_br_ccopy_v2(trial_context);
+        iteration_durations[i*32+ki] = timed_call_uut(bit, trial_context);
         helper_stop(&run_context);
         destroy_trial_context(trial_context);
     }
@@ -39,8 +38,7 @@ global_teardown(&global_context);
 global_setup and global_teardown are used if iterations > 1, it allows the client to initialize the system state, start processes, threads, whatever they need. If they need to pass information between setup and teardown, store it in the state member of the ctx parameter.
 trial_setup and trial_inner_setup allow the client to initialize the system state further with finer control. This state is setup and torn down every iteration. trial_setup is called before the trial context is generated and trial_inner_setup is called for each bit of the key after the context is generated.
     ^ The client should not spawn processes or threads here, instead this should be used to setup state, any processes or threads should be performed in helper_start and helper_stop.
-      Do not free the arrays inside of the trial_context_t, you can modify
-helper_start and helper_stop can be used to initialize the system, spawn processes, etc...
+      Do not free the arrays inside of the trial_context_t, you can modify helper_start and helper_stop can be used to initialize the system, spawn processes, etc...
 */
 
 void global_setup(global_context_t* ctx);
