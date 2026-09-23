084cc7d7b9f70484a11bd783d16c57a1fe6c8f7f
aaron-jencks <aaronjencks@aol.com>
2026-09-22 20:01:36 -0400

    got display looking nice

--- automation-testing/display.py
+++ automation-testing/display.py
@@ -3,13 +3,12 @@ from collections import defaultdict
 import json
 import logging
 from pathlib import Path
-from typing import List, Tuple
+from typing import Any, Callable, Dict, List, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 from common import CollatedData
-
 
 logger = logging.getLogger(__name__)
 
@@ -19,18 +18,67 @@ def generate_array_stats(arr: List[List[float]]) -> Tuple[List[float], List[floa
     mins = []
     maxes = []
     for sequence in arr:
+        if len(sequence) == 0:
+            means.append(0)
+            mins.append(0)
+            maxes.append(0)
+            continue
         means.append(np.mean(sequence))
         mins.append(np.min(sequence))
         maxes.append(np.max(sequence))
     return means, mins, maxes
 
 
-def generate_bar_plot_w_errors(ax, labels: List, values: List[List[float]], legend_label: str | None = None):
+def generate_bar_plot_w_errors(ax, labels: List, values: List[List[float]], legend_label: str | None = None, bar_width: float | None = None):
     means, mins, maxes = generate_array_stats(values)
-    if legend_label is None:
-        ax.bar(labels, means, yerr=np.array([mins, maxes]))
-    else:
-        ax.bar(labels, means, yerr=np.array([mins, maxes]), label=legend_label)
+    kwargs: Dict[str, Any] = {
+        "yerr": np.array([mins, maxes]),
+        "error_kw": {
+            "ecolor": (0, 0, 0, 0.5),
+            "capsize": 5
+        }
+    }
+    if legend_label is not None:
+        kwargs["label"] = legend_label
+    if bar_width is not None:
+        kwargs["width"] = bar_width
+    ax.bar(labels, means, **kwargs)
+
+
+DATA_EXTRACTOR = Callable[[str, str], List[float]]
+
+
+def generate_side_stacked_bar_plot(
+        ax,
+        x_labels: List[str], agent_labels: List[str],
+        extractor: Callable[[str, str], List[float]],
+        total_width: float = 2.0
+):
+    bar_width = total_width / len(agent_labels)
+    x = np.arange(len(x_labels))
+    for ni, name in enumerate(x_labels):
+        values = [extractor(name, agent_name) for agent_name in agent_labels]
+        generate_bar_plot_w_errors(ax, x + ni * bar_width, values, legend_label=name, bar_width=bar_width)
+    ax.set_xticks(x)
+    ax.set_xticklabels(agent_labels)
+
+
+def setup_plot(
+        ax,
+        title: str, x_title: str, y_title: str,
+        legend: bool = True,
+        y_log: bool = False,
+):
+    if y_log:
+        ax.set_yscale("log")
+        ax.grid(True, which="major", axis="y", linestyle="-", alpha=0.4, zorder=0)
+        ax.grid(True, which="minor", axis="y", linestyle="--", alpha=0.2, zorder=0)
+        ax.set_axisbelow(True)
+    ax.set_title(title)
+    ax.set_xlabel(x_title)
+    ax.set_ylabel(y_title)
+    if legend:
+        ax.legend()
 
 
 def main():
@@ -45,21 +93,13 @@ def main():
         collation = CollatedData.model_validate(json.load(fp))
         stats = collation.results
 
-    fig, axes = plt.subplots(3, 3, figsize=(20, 15))
+    fig, axes = plt.subplots(3, 3, figsize=(20, 15), constrained_layout=True)
 
     for name, stat in stats.items():
-        means = []
-        mins = []
-        maxes = []
-        for iteration in stat.iteration_scores:
-            means.append(np.mean(iteration))
-            mins.append(np.min(iteration))
-            maxes.append(np.max(iteration))
-        axes[0, 0].plot(means, yerr=np.array([mins, maxes]), label=name)
-    axes[0, 0].set_title("Implementation Score Over Time")
-    axes[0, 0].set_xlabel("Iteration")
-    axes[0, 0].set_ylabel("Score")
-    axes[0, 0].legend()
+        means, mins, maxes = generate_array_stats(stat.iteration_scores)
+        x = np.arange(len(means))
+        axes[0, 0].errorbar(x, means, yerr=np.array([mins, maxes]), fmt="o-", capsize=5, label=name)
+    setup_plot(axes[0, 0], "Implementation Score Over Time", "Iteration", "Score")
 
     names = list(stats.keys())
 
@@ -69,9 +109,7 @@ def main():
         for name in names
     ]
     generate_bar_plot_w_errors(axes[0, 1], names, values)
-    axes[0, 1].set_title("Iterations to Success")
-    axes[0, 1].set_xlabel("Configuration")
-    axes[0, 1].set_ylabel("Iterations")
+    setup_plot(axes[0, 1], "Iterations to Success", "Configuration", "Iterations", legend=False)
 
     # Probability of success at k
     iteration_count = 0
@@ -84,91 +122,63 @@ def main():
     for name in names:
         for succ in stats[name].success_iterations:
             values[name][succ:] += 1
-        values[name] /= config.candidate_iterations
+        values[name] /= collation.candidate_iterations
 
     x = np.arange(iteration_count)
     total_width = 0.15*len(names)
     for ni, name in enumerate(names):
-        axes[0, 2].bar(x + ni*0.15, values[name], total_width, label=name)
+        axes[0, 2].bar(x + ni*0.15, values[name], 0.15, label=name)
 
-    axes[0, 2].set_title("Probability of Success")
-    axes[0, 2].set_ylabel("Probability")
-    axes[0, 2].set_xlabel("Iteration")
-    axes[0, 2].legend()
+    setup_plot(axes[0, 2], "Probability of Success", "Iteration", "Probability")
 
     # Token Usage
     agent_names = list(set(list(stats[names[0]].success_token_usage.keys()) + list(stats[names[0]].failure_token_usage.keys())))
 
-    total_width = 2.0
-    bar_width = total_width / len(agent_names)
+    total_width = 0.33
 
-    x = np.arange(len(agent_names))
+    generate_side_stacked_bar_plot(
+        axes[1, 0], names, agent_names,
+        lambda name, agent_name: stats[name].success_token_usage[agent_name].input_count,
+        total_width=total_width
+    )
+    setup_plot(axes[1, 0], "Input Token Usage Upon Success", "Agent", "Tokens (log)", y_log=True)
 
-    for ni, name in enumerate(names):
-        values = [stats[name].success_token_usage[agent_name].input_count for agent_name in agent_names]
-        generate_bar_plot_w_errors(axes[1, 0], x + ni * bar_width, values, legend_label=name)
-    axes[1, 0].set_xticks(x)
-    axes[1, 0].set_xticklabels(agent_names)
-    axes[1, 0].set_title("Input Token Usage Upon Success")
-    axes[1, 0].set_ylabel("Token Usage")
-    axes[1, 0].set_xlabel("Agent")
-    axes[1, 0].legend()
+    generate_side_stacked_bar_plot(
+        axes[1, 1], names, agent_names,
+        lambda name, agent_name: stats[name].success_token_usage[agent_name].output_count,
+        total_width=total_width
+    )
+    setup_plot(axes[1, 1], "Output Token Usage Upon Success", "Agent", "Tokens (log)", y_log=True)
 
-    for ni, name in enumerate(names):
-        values = [stats[name].success_token_usage[agent_name].output_count for agent_name in agent_names]
-        generate_bar_plot_w_errors(axes[1, 1], x + ni * bar_width, values, legend_label=name)
-    axes[1, 1].set_xticks(x)
-    axes[1, 1].set_xticklabels(agent_names)
-    axes[1, 1].set_title("Output Token Usage Upon Success")
-    axes[1, 1].set_ylabel("Token Usage")
-    axes[1, 1].set_xlabel("Agent")
-    axes[1, 1].legend()
+    generate_side_stacked_bar_plot(
+        axes[1, 2], names, agent_names,
+        lambda name, agent_name: np.array(stats[name].success_token_usage[agent_name].output_count) + np.array(stats[name].success_token_usage[agent_name].input_count),
+        total_width=total_width
+    )
+    setup_plot(axes[1, 2], "Total Token Usage Upon Success", "Agent", "Tokens (log)", y_log=True)
 
-    for ni, name in enumerate(names):
-        values = [
-            np.array(stats[name].success_token_usage[agent_name].output_count) + np.array(stats[name].success_token_usage[agent_name].input_count)
-            for agent_name in agent_names
-        ]
-        generate_bar_plot_w_errors(axes[1, 2], x + ni * bar_width, values, legend_label=name)
-    axes[1, 2].set_xticks(x)
-    axes[1, 2].set_xticklabels(agent_names)
-    axes[1, 2].set_title("Total Token Usage Upon Success")
-    axes[1, 2].set_ylabel("Token Usage")
-    axes[1, 2].set_xlabel("Agent")
-    axes[1, 2].legend()
+    generate_side_stacked_bar_plot(
+        axes[2, 0], names, agent_names,
+        lambda name, agent_name: stats[name].failure_token_usage[agent_name].input_count,
+        total_width=total_width
+    )
+    setup_plot(axes[2, 0], "Input Token Usage Upon Failure", "Agent", "Tokens (log)", y_log=True)
 
-    for ni, name in enumerate(names):
-        values = [stats[name].failure_token_usage[agent_name].input_count for agent_name in agent_names]
-        generate_bar_plot_w_errors(axes[2, 0], x + ni * bar_width, values, legend_label=name)
-    axes[2, 0].set_xticks(x)
-    axes[2, 0].set_xticklabels(agent_names)
-    axes[2, 0].set_title("Input Token Usage Upon Failure")
-    axes[2, 0].set_ylabel("Token Usage")
-    axes[2, 0].set_xlabel("Agent")
-    axes[2, 0].legend()
+    generate_side_stacked_bar_plot(
+        axes[2, 1], names, agent_names,
+        lambda name, agent_name: stats[name].failure_token_usage[agent_name].output_count,
+        total_width=total_width
+    )
+    setup_plot(axes[2, 1], "Output Token Usage Upon Failure", "Agent", "Tokens (log)", y_log=True)
 
-    for ni, name in enumerate(names):
-        values = [stats[name].failure_token_usage[agent_name].output_count for agent_name in agent_names]
-        generate_bar_plot_w_errors(axes[2, 1], x + ni * bar_width, values, legend_label=name)
-    axes[2, 1].set_xticks(x)
-    axes[2, 1].set_xticklabels(agent_names)
-    axes[2, 1].set_title("Output Token Usage Upon Failure")
-    axes[2, 1].set_ylabel("Token Usage")
-    axes[2, 1].set_xlabel("Agent")
-    axes[2, 1].legend()
+    generate_side_stacked_bar_plot(
+        axes[2, 2], names, agent_names,
+        lambda name, agent_name: np.array(stats[name].failure_token_usage[agent_name].output_count) + np.array(stats[name].failure_token_usage[agent_name].input_count),
+        total_width=total_width
+    )
+    setup_plot(axes[2, 2], "Total Token Usage Upon Failure", "Agent", "Tokens (log)", y_log=True)
 
-    for ni, name in enumerate(names):
-        values = [
-            np.array(stats[name].failure_token_usage[agent_name].output_count) + np.array(stats[name].failure_token_usage[agent_name].input_count)
-            for agent_name in agent_names
-        ]
-        generate_bar_plot_w_errors(axes[2, 2], x + ni * bar_width, values, legend_label=name)
-    axes[2, 2].set_xticks(x)
-    axes[2, 2].set_xticklabels(agent_names)
-    axes[2, 2].set_title("Total Token Usage Upon Failure")
-    axes[2, 2].set_ylabel("Token Usage")
-    axes[2, 2].set_xlabel("Agent")
-    axes[2, 2].legend()
+    plt.show()
 
 
 if __name__ == "__main__":
